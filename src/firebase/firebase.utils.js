f5a1254e8d4503880dde945458493212585593e6
Poretsky234 <egor.poretsky@yandex.ru>
2021-09-30 20:41:07 +0300

    integrated firebase into appand crated sign in page with sign in component and sign in google

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+const config = {
+    apiKey: "AIzaSyAVsUhN7_mzDltM00XA4vsZ_Vl5_fpGxBo",
+  authDomain: "clothes-db-2ed94.firebaseapp.com",
+  projectId: "clothes-db-2ed94",
+  storageBucket: "clothes-db-2ed94.appspot.com",
+  messagingSenderId: "137846267621",
+  appId: "1:137846267621:web:a9c0ebb44d28aed096fe0c",
+  measurementId: "G-E14K7W2ZWK"
+}
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ promt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
